845c208772254913ad4bccca74ca8000eeee5765
iamrafabrito <rafaabrito7@gmail.com>
2023-04-18 10:51:13 -0300

    feat: adding dynamic values to posts

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,41 @@ import styles from './App.module.css';
 
 import './global.css';
 
+// author: { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/rafaelbrito7.png',
+      name: 'Rafael Brito',
+      role: 'Web Developer @Mouts TI'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00')
+  },
+]
+
 export function App() {
   return (
     <>
@@ -16,14 +51,16 @@ export function App() {
           sidebar
         </Sidebar>
         <main>
-          <Post 
-            author="John Doe"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          />
-          <Post
-            author="Jane Doe"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          />
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
